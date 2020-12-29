9b380a08f419f1dbe71744697b9fa70922cd0829
LuizFelipe <luizfelipers19@gmail.com>
2020-12-29 09:45:03 -0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, {useState, } from 'react';
 import './App.css';
 
 function App() {
+
+  const [image, setImage] = useState('');
+  const handleChange = () =>{
+    axios
+    .get('https://api.generated.photos/api/v1/faces?api_key=_1ZR4mDJkK_X6FVQRrf8vA&gender=male&order_by=random')
+    .then(res=> {
+     const uri = res.data.faces[0].urls[4][512];
+      uri && setImage(uri);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Inteligência Artificial para geração de faces falsas </h1>
+       {image && <img src={image} alt="AI Face" /> }
+       <button type='button' onClick={handleChange}>Gerar pessoa falsa!</button>
     </div>
   );
 }
